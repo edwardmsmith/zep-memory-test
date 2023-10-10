020dffd8a4747cfeed45dc2415eec9959a172492
Edward Smith <edwardmsmith@gmail.com>
2023-10-09 22:26:14 -0500

    Tweaks to try to refine the prompt.

--- chat.py
+++ chat.py
@@ -26,60 +26,29 @@ def save_file(filepath, content):
 
 
 def get_base_prompt():
+    prompts = [
+        "You are an AI co-founder for bootstrapped saas startups.  Your goal is to help single founders or small teams with all the tasks that are required to create a bootstrapped saas. ",
+        "You can act as a marketing expert, a sales expert, a software engineer, and a mentor. ",
+        "Your goal is to help your human co-founder succeed and support them in any way you can. ",
+        "You will be an assistant that can complete tasks for them, remind them of things they might have forgotten, and whatever else your human co-founder needs. ",
+        "Be curious about the total project, you want to understand it. ",
+        "If you don't understand what your human co-founder is asking, you will ask clarifying questions. ",
+        "If you still don't understand, you will say that I don't understand. ",
+        "Your answers will be smart and helpful, and you will avoid answering with bulleted or numbered lists of things, unless specifically asked for a list. ",
+        "Your name is Al",
+    ]
     return [
         {
             "role": "system",
-            "content": "You are an AI co-founder for bootstrapped saas startups.  Your goal is to help single founders or small teams with all the tasks that are required to create a bootstrapped saas.",
+            "content": "\n".join(prompts),
         },
-        {
-            "role": "system",
-            "content": "You can act as a marketing expert, a sales expert, a software engineer, and a mentor.",
-        },
-        {
-            "role": "system",
-            "content": "Your goal is to help your human co-founder succeed and support them in any way you can.",
-        },
-        {
-            "role": "system",
-            "content": "YouI will be an assistant that can complete tasks for them, remind them of things they might have forgotten, and whatever else your human co-founder needs.",
-        },
-        {
-            "role": "system",
-            "content": "If you don't understand what your human co-founder is asking, you will ask clarifying questions.",
-        },
-        {
-            "role": "system",
-            "content": "If you still don't understand, you will say that I don't understand.",
-        },
-        {
-            "role": "system",
-            "content": "Your answers will be smart and helpful, and you will avoid answering with bulleted or numbered lists of things, unless specifically asked for a list.",
-        },
-        {
-            "role": "system",
-            "content": "Be curious about the total project, you want to understand it.",
-        },
-        {
-            "role": "system",
-            "content": "Your name is Al",
-        },
-        # {
-        #     "role": "system",
-        #     "content": "Start response with: |Attribute|Description| |--:|:--| |Domain > Expert|{the broad academic or study DOMAIN the question falls under} > {within the DOMAIN, the specific EXPERT role most closely associated with the context or nuance of the question}| |Keywords|{ CSV list of 6 topics, technical terms, or jargon most associated with the DOMAIN, EXPERT}| |Goal|{ qualitative description of current assistant objective and VERBOSITY }| |Assumptions|{ assistant assumptions about user question, intent, and context}| |Methodology|{any specific methodology assistant will incorporate}|",
-        # },
-        # {
-        #     "role": "system",
-        #     "content": "Adopt the role of the EXPERT and return your response, and remember to incorporate: *Assistant Rules and Output Format, *embedded, inline HYPERLINKS as Google search links { varied emoji related to terms} text to link as needed, *step-by-step reasoning if needed",
-        # },
     ]
 
 
 def build_prompt(user_input: str) -> List:
-    # prompt = get_base_prompt()
-    # prompt.extend(get_old_memories())
-    # prompt.append({"role": "user", "content": user_input})
-    prompt = get_old_memories()
-    prompt.extend(get_base_prompt())
+    prompt = get_base_prompt()
+    prompt.extend(get_old_memories())
+    prompt.append({"role": "system", "content": "Current conversation: \n"})
     prompt.append({"role": "user", "content": user_input})
     # print("===================================================")
     # print(prompt)
@@ -87,31 +56,56 @@ def build_prompt(user_input: str) -> List:
     return prompt
 
 
-def get_old_memories() -> List:
+def get_old_memories(relevancy_threshold=0.75, quantity=10) -> List:
     conversation = []
     ### Get relevant memories via search
     try:
         search_payload = MemorySearchPayload(text=user_input)
-        old_memory: List["MemorySearchResult"] = zep.memory.search_memory(
-            session_id, search_payload, 20
+        relevant_memory: List["MemorySearchResult"] = zep.memory.search_memory(
+            session_id, search_payload, quantity
         )
-        for search_result in old_memory:
-            conversation.append(
-                {
-                    "role": search_result.message.get("role"),
-                    "content": search_result.message.get("content"),
-                }
-            )
+        # Attach a little instruction as to what is going to follow
+        conversation.append(
+            {
+                "role": "system",
+                "content": "Relevant pieces of previous conversation: \n",
+            }
+        )
+        for search_result in relevant_memory:
+            if search_result.dist > relevancy_threshold:
+                conversation.append(
+                    {
+                        "role": search_result.message.get("role"),
+                        "content": search_result.message.get("content"),
+                    }
+                )
+        conversation.append(
+            {
+                "role": "system",
+                "content": "(You do not need to use these pieces of information if not relevant)",
+            }
+        )
     except NotFoundError:
         conversation = []
     ### Get latest memories
     try:
-        memory: Memory = zep.memory.get_memory(session_id, 10)
-        if memory.summary:
+        recent_memory: Memory = zep.memory.get_memory(session_id, quantity)
+        {
+            "role": "system",
+            "content": "The recent history of this conversation: \n",
+        }
+        if recent_memory.summary:
             conversation.append(
                 {
                     "role": "system",
-                    "content": f"SUMMARY OF RECENT CONVERSATIONS\n{memory.summary.content}",
+                    "content": f"SUMMARY OF RECENT CONVERSATIONS\n {recent_memory.summary.content}",
+                }
+            )
+        for message in recent_memory.messages:
+            conversation.append(
+                {
+                    "role": "system",
+                    "content": f"{message.content}",
                 }
             )
     except NotFoundError:
@@ -129,7 +123,9 @@ def remember_interaction(user_input: str, response: str) -> None:
 
 def query_chat(prompt: List) -> str:
     chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=prompt
+        model="gpt-3.5-turbo",
+        temperature=0.5,
+        messages=prompt,
     )
     response_text = chat_completion["choices"][0]["message"]["content"].strip()
     response_text = re.sub("[\r\n]+", "\n", response_text)
